03b83a9d563e7342cf72e5c6a44fea4d4f0d646b
therafaelsilva <rafaelsilva.rx@gmail.com>
2023-12-18 21:49:28 -0300

    feature: sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -9,11 +11,14 @@ export function Sidebar() {
       />
     
       <div className={styles.profile}>
-        <strong>Diego Fernandes</strong>
+        <img className={styles.avatar} src="https://github.com/therafaelsilva.png" />
+        
+        <strong>Rafael Silva</strong>
         <span>Web Developer</span>
 
         <footer>
           <a href="#">
+            <PencilLine size={20}/>
             Editar seu perfil
           </a>
         </footer>
